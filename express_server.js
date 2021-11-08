b89ed5c218dfbe99ef205e2a11dfff387aa8fa29
Ammar <ammar.kusow2@gmail.com>
2021-11-08 18:04:04 -0500

    commited

--- express_server.js
+++ express_server.js
@@ -3,14 +3,34 @@ const { url } = require('inspector');
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
-
+const bodyParser = require("body-parser");
+const { match } = require('assert');
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': "http://www.google.com"
 };
+function generateRandomString() {
+let array = [1,2,3,'a','v','n','l',0]
+let str = ''
+while (str.length < 6){
+  str += array[Math.floor(Math.random() * 6)]
+}
+return str
+}
+    
+app.post("/urls", (req, res) => {
+  //console.log('i got here')
+  console.log(generateRandomString())
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get('/', (req,res)=>{ 
   res.send("hello")
